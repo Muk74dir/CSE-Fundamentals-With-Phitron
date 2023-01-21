beb50abe9d8fa84c88c220392388d79225ba5735
Muk74dir <muktadir.sarker99@gmail.com>
2023-01-21 15:26:45 +0600

    Week1 Completed

--- Basic Data Structures and Problem Solving Part-II/Week - 01  Introduction to C++/Module_03 - C++ Class & Object/DynamicObject.cpp
+++ Basic Data Structures and Problem Solving Part-II/Week - 01  Introduction to C++/Module_03 - C++ Class & Object/DynamicObject.cpp
@@ -1,1 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class Student{
+public:
+    string name;
+    int age;
+    int id;
+
+    Student(string name, int age, int id)
+    {
+        this->name = name;
+        this->age = age;
+        this->id = id;
+    }
+    void print_info(){
+        cout<<name<<endl;
+        cout<<age<<endl;
+        cout<<id<<endl;
+    }
+};
+
+int main()
+{
+    Student *s = new Student("Muktadir", 22, 22101147);
+    s->print_info();
+    (*s).print_info();
+
+    return 0;
+}

e63aaa286bce4ee10c3a78467cf979a9fac4cabd
Muk74dir <muktadir.sarker99@gmail.com>
2023-02-04 23:27:21 +0600

    WEEK-6 Done

---
+++ Basic Data Structures and Problem Solving Part-II/Week - 06 Introduction to Stack & Template in C++/Module_23 - Theory Assignment 02/ANSWER - 3.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template <class T>
+class Stack{
+public:
+    Stack()
+    {
+
+    }
+
+    T print(T value)
+    {
+        return value;
+    }
+};
+int main()
+{
+    Stack<int> int_stack;
+    cout<<int_stack.print(5)<<endl;
+
+    Stack<char> char_stack;
+    cout<<char_stack.print('M')<<endl;
+
+    Stack<double> real_stack;
+    cout<<real_stack.print(10.99)<<endl;
+}

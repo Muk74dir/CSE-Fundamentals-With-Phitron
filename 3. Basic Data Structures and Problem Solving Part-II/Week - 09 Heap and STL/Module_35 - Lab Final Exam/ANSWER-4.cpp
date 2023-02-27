80dab5f2bcde654bec36db49c8aa4948ea2465a7
Muk74dir <muktadir.sarker99@gmail.com>
2023-02-28 00:10:44 +0600

    Answer-4Done

--- 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-4.cpp
+++ 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-4.cpp
@@ -1,1 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+int main()
+{
+    stack<int>st;
+    string input;cin>>input;
+
+    for(int i=0; i<input.size(); i++)
+    {
+        char now = input[i];
+        if(now == '(' || now == '{' || now == '['){
+            st.push(now);
+        }
+        else
+        {
+            if(st.empty()){
+                st.push(now);
+                break;
+            }
+
+            else
+            {
+                if(now == ')' && st.top() == '('){
+                    st.pop();
+                }
+                else if(now == '}' && st.top() == '{'){
+                    st.pop();
+                }
+                else if(now == ']' && st.top() == '['){
+                    st.pop();
+                }
+                else
+                    break;
+            }
+        }
+    }
+
+    if(st.empty())cout<<"YES"; else cout<<"NO";
+
+    return 0;
+}

9b31e30162b99df8ef63570e71faec10d3757edc
Muk74dir <muktadir.sarker99@gmail.com>
2023-03-03 17:23:37 +0600

    ExamSubmitted

--- 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-6.cpp
+++ 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-6.cpp
@@ -3,40 +3,30 @@ using namespace std;
 
 int main()
 {
-    deque<int> dq;
-    deque<int>tq;
     int t;cin>>t;
-    int temp;
+    deque<int>input;
+    deque<int>ans;
     for(int i=0; i<t; i++)
     {
-        cin>>temp;
-        if(dq.empty()){
-            dq.push_back(temp);
+        int temp;cin>>temp;
+        input.push_back(temp);
+    }
+    while(!input.empty()){
+        if(input.front() < input.back()){
+            ans.push_back(input.front());
+            input.pop_front();
         }
-
-        else if(dq.back() < temp){
-            dq.push_back(temp);
-        }
-
-        else if(dq.back() > temp){
-            while(dq.back() > temp){
-                tq.push_back(dq.back());
-                dq.pop_back();
-            }
-            dq.push_back(temp);
+        else
+        {
+            ans.push_back(input.back());
+            input.pop_back();
         }
     }
 
-    for(int i=0; i<tq.size(); i++){
-        dq.push_back(tq.front());
+    for(int j=0; j<t; j++){
+        cout<<ans.front()<<" ";
+        ans.pop_front();
     }
-
-
-    for(int i=0; i<dq.size(); i++){
-        cout<<dq.front()<<" ";
-        dq.pop_front();
-    }
-
 
     return 0;
 }
--- 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-9.cpp
+++ 3. Basic Data Structures and Problem Solving Part-II/Week - 09 Heap and STL/Module_35 - Lab Final Exam/ANSWER-9.cpp
@@ -3,6 +3,22 @@ using namespace std;
 
 int main()
 {
+    int t;cin>>t;
+    map<string, int>mp;
+    for(int i=0; i<t; i++)
+    {
+        string temp;
+        cin>>temp;
+        if(mp.count(temp))
+        {
+            cout<<mp[temp]<<endl;
+        }
+        else
+        {
+            cout<<-1<<endl;
+        }
+        mp[temp] = i;
+    }
 
     return 0;
 }

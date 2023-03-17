ff63c9ab9f38d66e155150a9a190308d5dd7c99a
Muk74dir <muktadir.sarker99@gmail.com>
2023-03-17 17:05:44 +0600

    Module3_ongoing

---
+++ 4. Introduction to Algorithms and Problem Solving Part-III/WEEK - 01 Introduction to Graph Algorithm/Module_03 - LabClass01/AdjacencyMatrix.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+/*
+    Graph Representation
+    ====================
+
+    Topic : Adjacency Matrix [2D Array]
+
+    0........1
+            / \
+           2...3
+
+    # Total Node : 4
+    # Total Edge : 4
+    # Undirected + Unweighted
+
+    Matrix  : Nodes * Nodes : 4x4
+
+    Matrix[i][j] = 1 if(i->j has a edge)
+
+    0->1
+    1->0
+    1->2
+    2->1
+    1->3
+    3->1
+    2->3
+    3->2
+
+            Time Complexity -> O(n^2) :::: Space Complexity -> O(n^2)
+*/
+
+int main()
+{
+    int nodes = 4; //O(1)
+    int matrix[nodes][nodes]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; //O(n^2)     :: O(n^2)
+
+
+    //O(E) E -> Edge , E -> n^2 === O(n^2)
+    // A ---- B
+    // n ---- n
+    // n x n = n^2
+    //
+    matrix[0][1] = 1; //O(1)
+    matrix[1][0] = 1;
+
+    matrix[1][2] = 1;
+    matrix[2][1] = 1;
+
+    matrix[1][3] = 1;
+    matrix[3][1] = 1;
+
+    matrix[2][3] = 1;
+    matrix[3][2] = 1;
+
+    for(int i=0; i<nodes; i++){     //O(n^2)
+        for(int j=0; j<nodes; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    return 0;
+}

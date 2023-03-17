72899b20f28dd0fbd277852f8e6e713cf6f4d3ba
Muk74dir <muktadir.sarker99@gmail.com>
2023-03-18 00:41:12 +0600

    Module3Done

--- 4. Introduction to Algorithms and Problem Solving Part-III/WEEK - 01 Introduction to Graph Algorithm/Module_03 - LabClass01/EdgeList.cpp
+++ 4. Introduction to Algorithms and Problem Solving Part-III/WEEK - 01 Introduction to Graph Algorithm/Module_03 - LabClass01/EdgeList.cpp
@@ -12,25 +12,73 @@ using namespace std;
 
     # Total Node : 4
     # Total Edge : 4
-    # Undirected + Unweighted
+    # Unweighted :
 
-    Undirected:
-    0 -> 1
-    1 -> 0, 2, 3
-    2 -> 1, 3
-    3 -> 1, 2
+    [
+        [0, 1]
+        [1, 2]
+        [1, 3]
+        [2, 3]
+    ]
 
-    Directed :
-      5
-    0-->1  ::=   0 = {1, 5}
+    # Weighted :
+    [
+        [0, 1, 5(W)]
+        [1, 2, 9]
+        [1, 3, 4]
+        [2, 3, 2]
+    ]
 
-      2
-    1-->2
-
-      8
-    2-->3
-
-      9
-    3-->1
         Time Complexity -> O(n^2) :::: Space Complexity -> O(n^2)
 */
+
+int main()
+{
+    int nodes = 4;
+    vector<vector<int>>edge_list;
+
+    /*
+        OR,
+    vector<vector<int>>edge_list = {
+        {0, 1},
+        {1, 2},
+        {1, 3},
+        {2, 3}
+
+    };
+    */
+
+
+    //Time : O(E) WorstCase : O(n^2), Space : O(E)
+    // Unweighted
+    edge_list.push_back({0, 1});
+    edge_list.push_back({1, 2});
+    edge_list.push_back({1, 3});
+    edge_list.push_back({2, 3});
+
+
+    for(int i=0; i<edge_list.size(); i++){
+        cout<<"From : "<<edge_list[i][0]<<" TO : "<<edge_list[i][1]<<endl;
+    }
+
+
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+
+
+    // Weighted :
+    vector<vector<int>>edge_list2 =
+    {
+        {0, 1, 6},
+        {1, 2, 2},
+        {1, 3, 9},
+        {2, 3, 4}
+    };
+
+    for(int i=0; i<edge_list2.size(); i++){
+        cout<<"From : "<<edge_list2[i][0]<<" TO : "<<edge_list2[i][1]<<" W. Value -> "<<edge_list2[i][2]<<endl;
+    }
+
+    return 0;
+}

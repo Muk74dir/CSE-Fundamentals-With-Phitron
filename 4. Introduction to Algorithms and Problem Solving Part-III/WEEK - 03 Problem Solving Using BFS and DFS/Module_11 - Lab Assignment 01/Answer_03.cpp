a3251536e5bfdea34a6c53b73a282830d4aa767f
Muk74dir <muktadir.sarker99@gmail.com>
2023-03-27 02:04:02 +0600

    Answer3Solved

--- 4. Introduction to Algorithms and Problem Solving Part-III/WEEK - 03 Problem Solving Using BFS and DFS/Module_11 - Lab Assignment 01/Answer_03.cpp
+++ 4. Introduction to Algorithms and Problem Solving Part-III/WEEK - 03 Problem Solving Using BFS and DFS/Module_11 - Lab Assignment 01/Answer_03.cpp
@@ -1,1 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+const int N = 1e5;
+vector<int>adj_list[N];
+int visited[N];
+
+bool dfs(int src)
+{
+    visited[src] = 1;
+
+    for(auto nxt : adj_list[src]){
+        if(visited[nxt] == 0){
+            if(dfs(nxt)){
+                return true;
+            }
+        }
+        else if(visited[nxt] == 1){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main()
+{
+    int v, e;cin>>v>>e;
+
+    for(int i=0; i<e; i++){
+        int f, t;cin>>f>>t;
+        adj_list[f].push_back(t);
+    }
+
+    bool is_cyclic = false;
+
+    for(int i=0; i<v; i++){
+        if(visited[i] == 0){
+            if(dfs(i)){
+                is_cyclic = true;
+                break;
+            }
+        }
+    }
+
+    if(is_cyclic){
+        cout<<"YES"<<endl;
+    }
+    else
+        cout<<"NO"<<endl;
+
+    return 0;
+}

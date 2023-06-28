6cd97345c3a60a8c2c40e9c5cc699dc6332a690c
Muk74dir <muktadir.sarker99@gmail.com>
2023-06-28 18:35:18 +0600

    Module05_Ongoing

---
+++ 6. Database Management [MySQL]/WEEK - 02 Database Table and Operators/Module_05 - Database Table/INSERT_INTO_TABLE.sql
@@ -0,0 +1,18 @@
+DROP DATABASE PHITRON;
+CREATE DATABASE PHITRON;
+USE PHITRON;
+
+CREATE TABLE STUDENT (
+	Roll INT,
+	_Name VARCHAR(30), 
+	ContactNo CHAR(11),
+	Email VARCHAR(30)
+);
+
+INSERT INTO Student(Roll, _Name, ContactNo, Email)
+VALUES (101, "Rahim", "01871727699", "rahim@gmail.com");
+
+INSERT INTO Student(Roll, _Name, Email)
+VALUES (102, "Ayush", "ayush@gmail.com");
+
+SELECT * FROM STUDENT;

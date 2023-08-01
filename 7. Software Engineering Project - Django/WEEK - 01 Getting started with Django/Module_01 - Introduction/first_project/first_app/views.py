e9da90d0158d794cc8b3d93e88efd813b522110a
Muk74dir <muktadir.sarker99@gmail.com>
2023-08-02 04:03:22 +0600

    Module01_Done

---
+++ 7. Software Engineering Project - Django/WEEK - 01 Getting started with Django/Module_01 - Introduction/first_project/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Hello World..!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>This is about page</h1>")

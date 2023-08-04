860d210838258246ace137fa9c29a277846d141b
Muk74dir <muktadir.sarker99@gmail.com>
2023-08-05 02:19:24 +0600

    AssignmentFinished

---
+++ 7. Software Engineering Project - Django/WEEK - 01 Getting started with Django/Module_04.5 - Assignment_01/Assignment01_Project/Assignment_Project/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('assignment_app/', include('assignment_app.urls')),
+]

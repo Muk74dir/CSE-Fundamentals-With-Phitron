de6a167968000f7e81608a1f28060d07dcd06721
Muk74dir <muktadir.sarker99@gmail.com>
2023-08-31 00:41:39 +0600

    Module_13Done

---
+++ 7. Software Engineering Project - Django/WEEK - 04 Final Project Bank_Management/Module_13 - RegistrationLoginLogoutFormView/Final Project Bank_Management/accounts/constants.py
@@ -0,0 +1,11 @@
+ACCOUNT_TYPE = (
+    ('Saving', 'Saving'),
+    ('Current', 'Current'),
+    ('Student', 'Student'),
+    ('Freelance','Freelance')
+)
+
+GENDER_TYPE = (
+    ('Male', 'Male'),
+    ('Female', 'Female')
+)
